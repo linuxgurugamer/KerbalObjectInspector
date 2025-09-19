deb2c726657785ee7019a03f31fd591ecff78796
linuxgurugamer@gmail.com <linuxgurugamer@gmail.com>
2025-09-19 10:11:23 -0400

    Updated version file to use github for new version info

--- KerbalObjectInspector/AssemblyVersion.cs
+++ KerbalObjectInspector/AssemblyVersion.cs
@@ -5,5 +5,5 @@
   
  using System.Reflection;
 
- [assembly: AssemblyVersion("1.1.8.2")]
- [assembly: AssemblyFileVersion("1.1.8.2")]
+ [assembly: AssemblyVersion("1.2.0.0")]
+ [assembly: AssemblyFileVersion("1.2.0.0")]
